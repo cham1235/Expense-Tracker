6cd5f39d60e7ceb48ac47389c390a24d38a558b0
cham1235 <hana171819@gmail.com>
2024-02-10 20:14:36 +0900

    ExpenseList　完成

--- Expense-Tracker/src/App.tsx
+++ Expense-Tracker/src/App.tsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import ExpenseList from "./components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "aaa",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 2,
+      description: "bbb",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 3,
+      description: "ccc",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 4,
+      description: "ddd",
+      amount: 10,
+      category: "Utilities",
+    },
+  ]);
   return (
     <>
-      <h1>Hello World</h1>
-      <ExpenseList />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </>
   );
 }
